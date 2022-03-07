50da2dba3511b3a375957031a7b7d693908ca659
Uğur KOÇMEN <86979659+ugurkocmen@users.noreply.github.com>
2022-03-07 15:08:15 +0300

    Update MultipleServer.py

--- MultipleServer.py
+++ MultipleServer.py
@@ -1,16 +1,13 @@
 '''
 
-DOCSTRING : It's application for chatting. I developed this application for Kaan Tekiner.
+DOCSTRING : It's application for chatting. I developed this application for my Cyber Security Teacher.
 
 How Can We Use?
 
-You need choose a username for application. If you do it, you can use it. When you choose your username, everybody
-can see your username.
+You need choose a username for users. If you do it, you can use it. When you choose your username, everybody can see your username.
+The server will forward all the messages to your screen for you.
 
 Developer : Uğur Koçmen
-Instagram : ukocmenn
-GitHub : github.com/ugurkocmen
-
 '''
 
 import socket
